e501769b523c3b49d5423534959420e11d858889
Kuchumof <kuchumof@gmail.com>
2023-05-20 16:53:45 +0500

    add in MainActivity.java random text click

--- app/src/main/java/com/example/bezdelnik/MainActivity.java
+++ app/src/main/java/com/example/bezdelnik/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.bezdelnik;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    Random random;
 
     TextView mainText;
 
@@ -17,14 +21,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mainText = findViewById(R.id.main_text);
+        random = new Random();
 
         ArrayList arrayList = new ArrayList();
+        arrayList.add("Text 0");
         arrayList.add("Text 1");
         arrayList.add("Text 2");
         arrayList.add("Text 3");
         arrayList.add("Text 4");
-        arrayList.add("Text 5");
 
-        mainText.setText(""+arrayList.get(3));
+
+        mainText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mainText.setText("" + arrayList.get(random.nextInt(arrayList.size())));
+            }
+        });
+
     }
 }
